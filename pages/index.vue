b3da503a90261f0cefc34661a0ec745653b48107
Robbie Reynolds <robbie.anton.reynolds@gmail.com>
2021-10-30 06:26:48 -0400

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,46 @@
 <script setup>
+	import { provideClient } from '@urql/vue'
+	import CharactersList from '../components/CharactersList.vue'
 
-    import { useNuxtApp } from '#app'
-    import { provideClient } from '@urql/vue';
-    import CharactersList from '../components/CharactersList.vue'
+	//const client = $urql
+	//console.log(client)
 
-    const nuxtApp = useNuxtApp()
-    console.log(nuxtApp)
+	provideClient({
+		url: 'https://rickandmortyapi.com/graphql',
+	})
 
-    const client = this.$urql
-    console.log(client)
+	const fooBar = () => {
+		// do something here
+		const a = 5
+		const b = 2
+		const z = a * b
+		// return something here
+		return {
+			data: {
+				limit: z,
+				start: a,
+				multiplier: b,
+				values: [a, b, z],
+			},
+		}
+	}
+</script>
 
-    provideClient({
-        url: 'https://rickandmortyapi.com/graphql',
-    })
-
-    const fooBar = () => {
-        // do something here
-        const a = 5
-        const b = 2
-        const z = a * b
-        // return something here
-        return  {
-            data: {
-                limit: z,
-                start: a,
-                multiplier: b,
-                values: [a, b, z]
-            }
-        }
-    }
-
+<script>
+	//const client = this.$urql
+	//console.log(client)
 </script>
 
 <template>
-    <div>
-        <p>foobar() => {{ fooBar() }}</p>
-        <Suspense>
-            <template #default>
-                <CharactersList />
-            </template>
-            <template #fallback>
-                <div>Loading...</div>
-            </template>
-        </Suspense>
-    </div>
+	<div>
+		<p>foobar() => {{ fooBar() }}</p>
+		<Suspense>
+			<template #default>
+				<CharactersList />
+			</template>
+			<template #fallback>
+				<div>Loading...</div>
+			</template>
+		</Suspense>
+	</div>
 </template>
--- plugins/urql.js
+++ plugins/urql.js
@@ -1,8 +1,35 @@
 import { defineNuxtPlugin } from '#app'
-import urql from '@urql/vue'
+import urql, {
+	cacheExchange,
+	createClient,
+	dedupExchange,
+	fetchExchange,
+	provideClient,
+	ssrExchange,
+} from '@urql/vue'
 
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.use(urql, {
-        url: 'https://rickandmortyapi.com/graphql',
-    })
+	const ssr = ssrExchange({
+		isClient: process.client,
+		initialState: process.client ? nuxtApp.payload['$urql'] : undefined,
+	})
+
+	if (process.server) {
+		nuxtApp.hooks.hook('app:rendered', () => {
+			// Is this the most appropriate hook?
+			nuxtApp.payload['$urql'] = ssr.extractData() // This will be available in window.__NUXT__.$urql. But there is also an empty window.__NUXT__.data and window.__NUXT__.state. What are those properties for / should you store custom data in there?
+		})
+	}
+
+	const client = createClient({
+		url: 'https://rickandmortyapi.com/graphql',
+		exchanges: [
+			dedupExchange,
+			cacheExchange,
+			ssr, // Add `ssr` in front of the `fetchExchange`
+			fetchExchange,
+		],
+	})
+
+	//nuxtApp.app.provide('$urql', client)
 })
